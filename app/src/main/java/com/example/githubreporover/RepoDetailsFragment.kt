abd24dcdc612692e76057fd34c390f435db1b462
KingAs6583 <starabbu007@gmail.com>
2024-03-14 16:10:57 +0530

    Added new Features Fix Bugs and add FavRepo option fix dark mode commit

--- app/src/main/java/com/example/githubreporover/RepoDetailsFragment.kt
+++ app/src/main/java/com/example/githubreporover/RepoDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.navigation.fragment.findNavController
@@ -68,8 +69,14 @@ class RepoDetailsFragment : Fragment() {
         binding.addFav.setOnClickListener {
             val repoEntity = repo.getRepoEntity()
 
-            if (!navigationArgs.isFavRepo) dbViewModel.addFavRepo(repoEntity)
-            else dbViewModel.removeFavRepo(repoEntity)
+            if (!navigationArgs.isFavRepo){
+                dbViewModel.addFavRepo(repoEntity)
+                Toast.makeText(requireContext(),"Add to Favourite",Toast.LENGTH_SHORT).show()
+            }
+            else {
+                dbViewModel.removeFavRepo(repoEntity)
+                Toast.makeText(requireContext(),"Remove From Favourite",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 
